d93d680c0d27cc709e0604db1d60b65d77f0d9af
01_KDT <01_KDT@BOOK-UP5F0RS3G7>
2026-01-13 11:15:54 +0900

    구현 클래스 2026-01-13

---
+++ src/com/sisa/HongKong.java
@@ -0,0 +1,41 @@
+package com.sisa;
+
+/*
+ * HongKong 클래스
+ * - MenuPan 인터페이스를 구현한 구현 클래스
+ * - 인터페이스에 선언된 모든 추상 메서드를 반드시 구현해야 함
+ */
+public class HongKong implements MenuPan { 
+    // implements : 인터페이스의 구현 약속
+    
+
+    /*
+     * menu1()
+     * - MenuPan 인터페이스에 선언된 메서드
+     * - 메뉴판 전체를 출력하는 기능
+     */
+  
+    public void menu1() {
+        System.out.println("메뉴판 출력");
+    }
+
+    /*
+     * jjamppong()
+     * - MenuPan 인터페이스에 선언된 메서드
+     * - 짬뽕 메뉴 출력
+     */
+    public void jjamppong() {
+        System.out.println("짬뽕");
+    }
+
+    /*
+     * bokkeumbap()
+     * - MenuPan 인터페이스에 선언된 메서드
+     * - 볶음밥 메뉴 출력
+     */
+ 
+    public void bokkeumbap() {
+        System.out.println("볶음밥");
+    }
+
+}
---
+++ src/com/sisa/MenuPan.java
@@ -0,0 +1,15 @@
+package com.sisa;
+
+public interface MenuPan {
+
+	/* public static final */
+	int MAX = 100; // 상수는 대문자 관례
+	// 상수(final) 는 반드시 명시해야 함.
+
+	void menu1();
+
+	void jjamppong();
+
+	void bokkeumbap();
+
+}
